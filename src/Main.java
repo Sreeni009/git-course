cf603c8b780a66749b705e3d7e50198dba7cfcec
Sreeni <sreenidaggolu@gmail.com>
2025-04-02 14:36:06 +0530

    Sreenivasulu commit for this project

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world -> Sreenivas!");
+        System.out.println("Hello world -> Sreenivasulu!");
     }
 }
