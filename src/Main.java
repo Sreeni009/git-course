1821dbc96660047b8cc4dcd7ce570c4f2d250adb
Sreeni <sreenidaggolu@gmail.com>
2025-04-02 15:45:03 +0530

    Two methods

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world -> Sreenivasulu!");
+        System.out.println("Hello world -> Sreenivasulu daggolu!");
+    }
+    public static void printSomething(){
+        System.out.println("welcom world -> Sreenivasulu daggolu!");
     }
 }
